3009cdd1a539baf22a908d5a01d0f5d58628f599
Darren Daly <archerydwd@gmail.com>
2015-03-12 15:59:02 +0000

    Update Flask_Blog_50000_Users.scala

--- user-files/simulations/blog/Flask_Blog_50000_Users.scala
+++ user-files/simulations/blog/Flask_Blog_50000_Users.scala
@@ -16,7 +16,7 @@ class Flask_Blog_50000_Users extends Simulation {
 	val users = scenario("Users").exec(Read.read)
 
 	val httpConf = http 
-		.baseURL("http://127.0.0.1:5000")
+		.baseURL("http://127.0.0.1")
 		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 		.doNotTrackHeader("1")
 		.acceptLanguageHeader("en-US,en;q=0.5")
