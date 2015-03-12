ba14efe5df185dba114ca29b8ee6458a79bb0a2d
Darren Daly <archerydwd@gmail.com>
2015-03-12 15:59:42 +0000

    Update Ruby_On_Rails_Blog_300000_Users.scala

--- user-files/simulations/blog/Ruby_On_Rails_Blog_300000_Users.scala
+++ user-files/simulations/blog/Ruby_On_Rails_Blog_300000_Users.scala
@@ -16,7 +16,7 @@ class Ruby_On_Rails_Blog_300000_Users extends Simulation {
 	val users = scenario("Users").exec(Read.read)
 
 	val httpConf = http 
-		.baseURL("http://127.0.0.1:3000")
+		.baseURL("http://127.0.0.1")
 		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 		.doNotTrackHeader("1")
 		.acceptLanguageHeader("en-US,en;q=0.5")
