fa65681a15168ff807b8e60ddb5a4d68f20a7cc5
Darren Daly <archerydwd@gmail.com>
2015-03-12 16:00:19 +0000

    Update Chicago_Boss_Sakila_100_Users.scala

--- user-files/simulations/sakila/Chicago_Boss_Sakila_100_Users.scala
+++ user-files/simulations/sakila/Chicago_Boss_Sakila_100_Users.scala
@@ -25,7 +25,7 @@ class Chicago_Boss_Sakila_100_Users extends Simulation {
 	val users = scenario("Users").exec(Read.read)
 
 	val httpConf = http 
-		.baseURL("http://127.0.0.1:8001")
+		.baseURL("http://127.0.0.1")
 		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 		.doNotTrackHeader("1")
 		.acceptLanguageHeader("en-US,en;q=0.5")
